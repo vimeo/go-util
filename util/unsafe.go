3c4d8e8bcc4a966b9907a8d68f92a043a2daf841
Vittorio Giovara <vittorio.giovara@gmail.com>
2015-07-07 17:51:15 +0100

    Add unsafe pointer conversion
    Needed for example when cgo is not available (as in testing).

---
+++ util/unsafe.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+    "reflect"
+    "unsafe"
+)
+
+// Convert an unsafe pointer to a byte slice.
+// The input buffer has to remain valid for the whole life-cycle of the output
+// slice, and users are responsible for freeing the associated memory.
+func UnsafeToBytes(buffer unsafe.Pointer, length int) []byte {
+    var output []byte
+
+    bufconv     := (*reflect.SliceHeader)(unsafe.Pointer(&output))
+    bufconv.Data = uintptr(buffer)
+    bufconv.Len  = length
+    bufconv.Cap  = length
+
+    return output
+}
