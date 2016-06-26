a89a819bffda924bfdc2f64557a62c6b6826c3da
Justin Ruggles <justinr@vimeo.com>
2016-06-26 10:15:18 -0400

    Add util.Duration that is a JSON-friendly time.Duration
    Marshals and unmarshals to/from a string in the time.ParseDuration() and
    time.Duration.String() format.

--- util/time.go
+++ util/time.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+    "encoding/json"
+    "fmt"
     "time"
 )
 
@@ -13,4 +15,27 @@ func ClipDuration(v, min, max time.Duration) time.Duration {
     } else {
         return v
     }
+}
+
+// JSON-friendly time.Duration
+type Duration struct {
+    time.Duration
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) (err error) {
+    if b[0] == '"' {
+        sd := string(b[1 : len(b)-1])
+        d.Duration, err = time.ParseDuration(sd)
+        return
+    }
+
+    var id int64
+    id, err = json.Number(string(b)).Int64()
+    d.Duration = time.Duration(id)
+
+    return
+}
+
+func (d Duration) MarshalJSON() (b []byte, err error) {
+    return []byte(fmt.Sprintf(`"%s"`, d.String())), nil
 }
